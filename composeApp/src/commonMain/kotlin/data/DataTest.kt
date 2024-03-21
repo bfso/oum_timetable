adc78de06a5bb99844773a4768be45535bf6fb0d
werlen-nevio <LordNevi@outlook.com>
2024-03-21 14:56:38 +0100

    Add Commentary

--- composeApp/src/commonMain/kotlin/data/DataTest.kt
+++ composeApp/src/commonMain/kotlin/data/DataTest.kt
@@ -8,7 +8,7 @@ class DataTest : DataProvider {
         Match(team1 = Team(name = "Team 3"), team2 = Team(name = "Team 4")),
         Match(team1 = Team(name = "Team 5"), team2 = Team(name = "Team 6")),
         Match(team1 = Team(name = "Team 1"), team2 = Team(name = "Team 3")),
-        Match(team1 = Team(name = "Team 2"), team2 = Team(name = "Team 3")),
+        Match(team1 = Team(name = "Team 2"), team2 = Team(name = "Team 4")),
     )
 
     override fun getData(date: LocalDate): List<Match> {
