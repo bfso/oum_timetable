c2e56911e0a240e1403e882ae05bf50830de6ae9
werlen-nevio <LordNevi@outlook.com>
2024-03-21 15:36:42 +0100

    Edit TestData

--- composeApp/src/commonMain/kotlin/data/DataTest.kt
+++ composeApp/src/commonMain/kotlin/data/DataTest.kt
@@ -3,18 +3,28 @@ package data
 import java.time.LocalDate
 
 class DataTest : DataProvider {
-    val matches: List<Match> = listOf(
-        Match(team1 = Team(name = "Team 1"), team2 =  Team(name = "Team 2")),
-        Match(team1 = Team(name = "Team 3"), team2 = Team(name = "Team 4")),
-        Match(team1 = Team(name = "Team 5"), team2 = Team(name = "Team 6")),
-        Match(team1 = Team(name = "Team 1"), team2 = Team(name = "Team 3")),
-        Match(team1 = Team(name = "Team 2"), team2 = Team(name = "Team 4")),
+    val teams: List<Team> = listOf(
+        Team(name = "Team 1"),
+        Team(name = "Team 2"),
+        Team(name = "Team 3"),
+        Team(name = "Team 4"),
+        Team(name = "Team 5"),
+        Team(name = "Team 6")
     )
+
+    val matches: List<Match> = listOf(
+        Match(team1 = findTeamByName("Team 1"), team2 = findTeamByName("Team 2")),
+        Match(team1 = findTeamByName("Team 3"), team2 = findTeamByName("Team 4")),
+        Match(team1 = findTeamByName("Team 5"), team2 = findTeamByName("Team 6")),
+        Match(team1 = findTeamByName("Team 1"), team2 = findTeamByName("Team 3")),
+        Match(team2 = findTeamByName("Team 6"), team1 = findTeamByName("Team 2"))
+    )
+
+    private fun findTeamByName(name: String): Team {
+        return teams.first { team -> team.name == name }
+    }
 
     override fun getData(date: LocalDate): List<Match> {
         return matches
     }
-
-
-
 }
